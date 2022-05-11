e3de850502d8a0d357b40f7357a98c3371dad34a
Miyukiinstant <miyuki.ram01@gmail.com>
2022-05-11 13:06:20 +0200

    FIrst commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardComponent } from './components/card/card.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatInputModule} from '@angular/material/input';
+import {MatSliderModule} from '@angular/material/slider';
+import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatGridListModule,
+    MatInputModule,
+    MatSliderModule,
+    HttpClientModule,
+    HttpClientJsonpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.sass']
+})
+
+export class CardComponent implements OnInit {
+
+  constructor(private httpclient:HttpClient) { }
+  imgSrc = ''
+  min = 0
+  max = 60
+  value = 0
+  diameter = 190
+  spinner = {
+    seconds: 0,
+    minutes: 0,
+    hours: 0,
+  }
+  date = new Date();
+  ngOnInit(): void {
+    fetch('http://localhost/').then(async response=>{
+      const result = await response.json()
+      this.imgSrc = result
+    })
+    setInterval(()=>{
+      this.date = new Date();
+      this.spinner.seconds = new Date().getSeconds() * 100 / 60
+      this.spinner.minutes = new Date().getMinutes() * 100 / 60
+      this.spinner.hours = new Date().getHours() * 100 / 24
+    },1000)
+  }
+
+}
